2a8bdadbde785119f1871534b8aaf5f83e895361
Ying Huang <huangyingw@gmail.com>
2020-07-25 00:52:10 -0400

    n

--- install.sh
+++ install.sh
@@ -7,7 +7,8 @@ wget -nc https://glide.sh/get | sh
 apt-get install -y mercurial
 glide install
 make
-cp -fv ./heketi /usr/local/bin 
-cp -fv ./client/cli/go/heketi-cli /usr/local/bin 
+cp -fv ./heketi /usr/local/bin
+cp -fv ./client/cli/go/heketi-cli /usr/local/bin
 heketi --version
 heketi-cli --version
+systemctl restart heketi
