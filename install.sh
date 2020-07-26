f754b90997cde4d7df9a45bd665c33e83d65512b
Ying Huang <huangyingw@gmail.com>
2020-07-26 00:54:24 -0400

    n

--- install.sh
+++ install.sh
@@ -3,12 +3,13 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-wget -nc https://glide.sh/get | sh
-apt-get install -y mercurial
-glide install
+#wget -nc https://glide.sh/get | sh
+#apt-get install -y mercurial
+#glide install
 make
 cp -fv ./heketi /usr/local/bin
 cp -fv ./client/cli/go/heketi-cli /usr/local/bin
 heketi --version
 heketi-cli --version
+cp -fv ./etc/heketi.json /etc/heketi
 systemctl restart heketi
