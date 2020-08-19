34f35b66d69ff7b40af3779b2c1babdd24feabf8
Ying Huang <huangyingw@gmail.com>
2020-08-19 12:59:03 -0400

    n

--- delete_nodes.sh
+++ delete_nodes.sh
@@ -5,6 +5,8 @@ cd "$SCRIPTPATH"
 
 for nodeid in $(heketi-cli node list | sed -n '/^Id:[a-f0-9]\+/p' | sed 's/Id:\([0-9a-f]\+\).*/\1/g')
 do
+    echo "nodeid --> $nodeid"
     ./delete_node.sh "$nodeid"
 done
+
 heketi-cli node list
